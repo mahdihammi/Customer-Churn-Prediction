6078eeb259c46d657f06a6c0e1b35e21f5588b0e
mahdihammi <mahdihammi12@gmail.com>
2023-10-25 14:19:41 +0200

    commit

--- Web App/app.py
+++ Web App/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
-
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 app = Flask(__name__)
 
 
-model = joblib.load('../my_model.joblib')
+model = joblib.load('../first_model.joblib')
 
-
+['tenure', 'InternetService_Fiber optic', 'InternetService_No',
+       'OnlineSecurity_No internet service',
+       'OnlineBackup_No internet service',
+       'DeviceProtection_No internet service',
+       'TechSupport_No internet service', 'StreamingTV_No internet service',
+       'StreamingMovies_No internet service', 'Contract_Two year',
+       'PaymentMethod_Electronic check']
 
 @app.route('/')
 def index():
@@ -19,23 +26,35 @@ def index():
 def predict():
     if request.method == 'POST' :
         tenure = request.form['tenure']
-        MonthlyCharges = request.form['MonthlyCharges']
-        TotalCharges = request.form['TotalCharges']
-        Fiber_optic = request.form['Fiber optic']
-        StreamingTV = request.form['StreamingTV internet service']
-        StreamingMovies = request.form['StreamingMovies internet service']
-        Contract_Two_year = request.form['Contract_Two year']
-        PaymentMethod_Electronic_check = request.form['PaymentMethod_Electronic_check']
-        data_input = [tenure,MonthlyCharges,TotalCharges,Fiber_optic,StreamingTV,StreamingMovies,Contract_Two_year,PaymentMethod_Electronic_check]
-        data_input = pd.to_numeric(data_input)
+        InternetService_Fiber = request.form['InternetService_Fiber optic']
+        InternetService_No = request.form['InternetService_No']
+        OnlineSecurity = request.form['OnlineSecurity_No internet service']
+        OnlineBackup_No = request.form['OnlineBackup_No internet service']
+        DeviceProtection_No = request.form['DeviceProtection_No internet service']
+        TechSupport_No = request.form['TechSupport_No internet service']
+        StreamTV = request.form['StreamingTV_No internet service']
+        StreamMovies = request.form['StreamingMovies_No internet service']
+        contrat = request.form['Contract_Two year']
+        pay = request.form['PaymentMethod_Electronic check']
+        
+        data_input = [[tenure,InternetService_Fiber,InternetService_No,OnlineSecurity,OnlineBackup_No,DeviceProtection_No,
+                      TechSupport_No,StreamTV, StreamMovies, contrat, pay]]
+        #data_input = pd.to_numeric(data_input)
+       
 
-        prediction = model.predict([data_input])
+        
+        scaler = MinMaxScaler()
+        X = scaler.fit_transform(data_input)
+
+        
+        
+        
+        
+        prediction = model.predict(X)
     
     return render_template('result.html', result = prediction)
 
 
-
-print("hello")
 
 if __name__ == '__main__':
     app.run(debug=True)
